8f4a0ec6db5898e9833ab2bdff8bf73c4d0ff164
hereiamsheel <solankisheel3084@gmail.com>
2026-04-16 15:59:13 +0530

    file structure changed.

--- src/objectdetection.py
+++ src/objectdetection.py
@@ -5,7 +5,9 @@ import time
 mp_objectron = mp.solutions.objectron
 mp_drawing = mp.solutions.drawing_utils
 
-cap = cv2.VideoCapture('shoe2.mp4')
+cap = cv2.VideoCapture('../test-resources/shoe2.mp4')
+cv2.namedWindow("Webcam", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("Webcam", 640, 480)
 
 with mp_objectron.Objectron(static_image_mode = False,
                             max_num_objects = 2,
@@ -16,8 +18,12 @@ with mp_objectron.Objectron(static_image_mode = False,
     while cap.isOpened():
         success, image = cap.read()
         start = time.time()
+        if not success:
+            print("Ignoring empty camera frame.")
+            continue
 
         image = cv2.flip(image,1)
+        image = cv2.resize(image, (640, 480))
         # Convert the BGR iamge to RGB
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
@@ -26,7 +32,7 @@ with mp_objectron.Objectron(static_image_mode = False,
         image.flags.writeable = False
         results = objectron.process(image)
 
-        image.flags.writeable - True
+        image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
         if results.detected_objects:
@@ -46,7 +52,7 @@ with mp_objectron.Objectron(static_image_mode = False,
                     (40,255,40),
                     2)
 
-        cv2.imshow("Web-Cam", image)
+        cv2.imshow("WebCam", image)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
